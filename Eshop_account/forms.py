fce06271f08af61f8a8d2e3ba9075b8dd8e85e91
sina <msinam2023@gmail.com>
2023-11-13 11:39:36 +0330

    add image html FOOTER HEADER REFRENSS

---
+++ Eshop_account/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+
+class LoginForms(forms.Form):
+    user_name=forms.CharField(
+        widget=forms.TextInput(attrs={'palcholder':"لطفا نلم کاربری خود را وارد کنید "}),
+        label="نام کاربری "
+    )
+
+password=forms.CharField(
+   widget=forms.PasswordInput(attrs={"palceholder":'لطفا کلمه عبو را وازد کنید'}),
+   label="کلمه عبور "
+
+)
