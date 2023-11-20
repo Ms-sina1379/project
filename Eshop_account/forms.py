f9b69a1399e253b8769ddc512ff79e5ae2b7c04b
sina <msinam2023@gmail.com>
2023-11-20 11:53:20 +0330

    login

--- Eshop_account/forms.py
+++ Eshop_account/forms.py
@@ -17,4 +17,20 @@ class LoginForms(forms.Form):
 )
     def clean_user_name(self):
         user_name=self.cleaned_data.get("user_name")
+        is_existe_user= User.objects.filter(username=user_name).exists()
+        if not is_existe_user:
+            raise forms.ValidationError("کاربر با مشخصات زیر ثبت نام نکرده است ")
         return user_name
+
+
+class Registerforms(forms.Form):
+    user_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': "لطفا نلم کاربری خود را وارد کنید "}),
+        label="نام کاربری "
+    )
+
+    password: CharField = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'لطفا رمز خود را وارد کنید '}),
+        label="کلمه عبور "
+
+    )
