2f4639c98cfa3aadd3da65e1c517b6c59381167e
sina <msinam2023@gmail.com>
2023-11-25 11:32:52 +0330

    save

--- Eshop_account/forms.py
+++ Eshop_account/forms.py
@@ -1,41 +1,110 @@
+# from django import forms
+# from django.contrib.auth.models import User
+# from django.forms import CharField
+#
+#
+# class LoginForms(forms.Form):
+#     user_name = forms.CharField(
+#         widget=forms.TextInput(attrs={'placeholder': "لطفا نلم کاربری خود را وارد کنید "}),
+#         label="نام کاربری "
+#     )
+#
+#
+#     password: CharField = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'placeholder': 'لطفا رمز خود را وارد کنید '}),
+#         label="کلمه عبور "
+#
+# )
+#     def clean_user_name(self):
+#         user_name=self.cleaned_data.get("user_name")
+#         is_existe_user= User.objects.filter(username=user_name).exists()
+#         if not is_existe_user:
+#             raise forms.ValidationError("کاربر با مشخصات زیر ثبت نام نکرده است ")
+#         return user_name
+#
+#
+# class Registerforms(forms.Form):
+#     user_name = forms.CharField(
+#         widget=forms.TextInput(attrs={'placeholder': "لطفا نلم کاربری خود را وارد کنید "}),
+#         label="نام کاربری "
+#         )
+#
+#     email= forms.CharField(
+#         widget=forms.EmailField(attrs={'placeholder': "لطفا ایمیل خود را وارد کنید "}),
+#         label="ایمیل"
+#         )
+#
+#     password: CharField = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'placeholder': 'لطفا رمز خود را وارد کنید '}),
+#         label="کلمه عبور "
+#
+#         )
+#    re_password: CharField = forms.CharField(
+#         widget=forms.PasswordInput(attrs={'placeholder': 'لطفا رمز خود را وارد کنید '}),
+#         label="کلمه عبور "
+#
+#     )
+#
+#
+# def clean_pssword (self):
+#      = self.cleaned_data.get("user_name")
+#     is_existe_user = User.objects.filter(username=user_name).exists()
+#     if not is_existe_user:
+#         raise forms.ValidationError("کاربر با مشخصات زیر ثبت نام نکرده است ")
+#     return user_name
+
+
+
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import CharField
 
-
 class LoginForms(forms.Form):
     user_name = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': "لطفا نلم کاربری خود را وارد کنید "}),
+        widget=forms.TextInput(attrs={'placeholder': "لطفا نام کاربری خود را وارد کنید "}),
         label="نام کاربری "
     )
 
-
-    password: CharField = forms.CharField(
+    password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'لطفا رمز خود را وارد کنید '}),
         label="کلمه عبور "
+    )
 
-)
     def clean_user_name(self):
-        user_name=self.cleaned_data.get("user_name")
-        is_existe_user= User.objects.filter(username=user_name).exists()
+        user_name = self.cleaned_data.get("user_name")
+        is_existe_user = User.objects.filter(username=user_name).exists()
         if not is_existe_user:
             raise forms.ValidationError("کاربر با مشخصات زیر ثبت نام نکرده است ")
         return user_name
 
-
 class Registerforms(forms.Form):
     user_name = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': "لطفا نلم کاربری خود را وارد کنید "}),
+        widget=forms.TextInput(attrs={'placeholder': "لطفا نام کاربری خود را وارد کنید "}),
         label="نام کاربری "
-        )
+    )
 
-    email= forms.CharField(
-        widget=forms.EmailField(attrs={'placeholder': "لطفا ایمیل خود را وارد کنید "}),
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': "لطفا ایمیل خود را وارد کنید "}),
         label="ایمیل"
-        )
+    )
 
-    password: CharField = forms.CharField(
+    password = forms.CharField(
         widget=forms.PasswordInput(attrs={'placeholder': 'لطفا رمز خود را وارد کنید '}),
         label="کلمه عبور "
-
     )
+
+    re_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'لطفا تکرار رمز خود را وارد کنید '}),
+        label="تکرار کلمه عبور "
+    )
+
+    def clean_password(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        re_password = cleaned_data.get("re_password")
+
+        if password and re_password and password != re_password:
+            raise forms.ValidationError("کلمه عبور و تکرار آن باید یکسان باشند")
+
+
+        return password
--- Eshop_account/views.py
+++ Eshop_account/views.py
@@ -1,30 +1,82 @@
-from django.shortcuts import render, redirect
-from .forms import LoginForms
-from django.contrib.auth import login,get_user_model, authenticate
+# from django.shortcuts import render, redirect
+# from .forms import LoginForms
+# from django.contrib.auth import login,get_user_model, authenticate
+#
+#
+# def login_user(request):
+#     # if request.user.is_authenticated:
+#     #     return redirect("/")
+#     Login_Form = LoginForms(request.POST or None)
+#     if Login_Form.is_valid():
+#         user_name = Login_Form.cleaned_data.get("user_name")
+#         password = Login_Form.cleaned_data.get("password")
+#         user = authenticate(username=user_name, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect("/")
+#         else:
+#             Login_Form.add_error('user_name', "کاربری با مشخصات زیر پیدا نشد ", )
+#         # print(Login_Form.cleaned_data)
+#     context = {
+#
+#         'login_form': Login_Form
+#     }
+#     return render(request, 'Eshop_account/login.html', context)
+#
+#
+# def register(request):
+#     if request.user.is_authenticated:
+#         return redirect("/")
+#
+#     Register_form = registerform(request.POST or None)
+#
+#     if register_form.is_valid():
+#         user_name = register_form.cleaned_data.get("user_name")
+#         password = register_form.cleaned_data.get("password")
+#         email = register_form.cleaned_data.get("email")
+#         # Create a new user
+#         User.objects.create_user(username=user_name, password=password, email=email)
+#         return redirect("/login")
+#
+#     context = {'register_form': register_form}
+#     return render(request, 'Eshop_account/register.html', context)
 
+
+from django.shortcuts import render, redirect
+from .forms import LoginForms, Registerforms
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
 def login_user(request):
     # if request.user.is_authenticated:
     #     return redirect("/")
-    Login_Form = LoginForms(request.POST or None)
-    if Login_Form.is_valid():
-        user_name = Login_Form.cleaned_data.get("user_name")
-        password = Login_Form.cleaned_data.get("password")
+    login_form = LoginForms(request.POST or None)
+    if login_form.is_valid():
+        user_name = login_form.cleaned_data.get("user_name")
+        password = login_form.cleaned_data.get("password")
         user = authenticate(username=user_name, password=password)
         if user is not None:
             login(request, user)
-            redirect("/")
+            return redirect("/")
         else:
-            Login_Form.add_error('user_name', "کاربری با مشخصات زیر پیدا نشد ", )
-        # print(Login_Form.cleaned_data)
-    context = {
-
-        'login_form': Login_Form
-    }
+            login_form.add_error('user_name', "کاربری با مشخصات زیر پیدا نشد")
+        # print(login_form.cleaned_data)
+    context = {'login_form': login_form}
     return render(request, 'Eshop_account/login.html', context)
 
-
 def register(request):
-    context = {}
+    if request.user.is_authenticated:
+        return redirect("/")
 
+    register_form = Registerforms(request.POST or None)
+
+    if register_form.is_valid():
+        user_name = register_form.cleaned_data.get("user_name")
+        password = register_form.cleaned_data.get("password")
+        email = register_form.cleaned_data.get("email")
+        # Create a new user
+        User.objects.create_user(username=user_name, password=password, email=email)
+        return redirect("/login")
+
+    context = {'register_form': register_form}
     return render(request, 'Eshop_account/register.html', context)
