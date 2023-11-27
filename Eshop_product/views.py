165e47b502ad4a908a67c45075ca817729789768
sina <msinam2023@gmail.com>
2023-11-27 11:18:45 +0330

    login

--- Eshop_product/views.py
+++ Eshop_product/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
 
 def products(request):
     context = {}
     return render(request, "product/product_list.html", context)
+
+
+class productlist(ListView):
+    template='product/product_list.html'
+
+
+    def get_queryset(self):
+        return products.objects.all()
+
+
