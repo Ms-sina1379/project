2c4f83e17d86d2d0f3d209ffee92e8d8cebe658a
sina <msinam2023@gmail.com>
2023-12-11 11:56:28 +0330

    save

--- Eshop_product/views.py
+++ Eshop_product/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from.models import Product
 # Create your views here.
 
 def Products(request):
     context = {}
     return render(request, "product/product_list.html", context)
 
-
 class ProductsList (ListView):
 
     template='product/product_list.html'
 
     def get_queryset(self):
-        return Products.object.all()
-
-
-
-
+        return Products.objects.all()
