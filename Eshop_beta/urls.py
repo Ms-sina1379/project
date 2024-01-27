de5f97d5d0299249e2fb70f647e8c738deec7f39
Ms-sina1379 <142148461+Ms-sina1379@users.noreply.github.com>
2024-01-27 12:53:56 +0330

    save

--- Eshop_beta/urls.py
+++ Eshop_beta/urls.py
@@ -1,24 +1,4 @@
-# from django.conf.urls.static import static
-# from django.contrib import admin
-# from django.urls import path, include
-#
-# from Eshop_beta import settings
-# from Eshop_beta.views import home_page, footer, header
-#
-# urlpatterns = [
-#
-#                   path('', home_page),
-#                   path('',include('Eshop_account.urls')),
-#                   path('',include('Eshop_product.urls')),
-#                   path('header/', header, name="header"),
-#                   path('footer', footer, name="footer"),
-#                   path('admin/', admin.site.urls),
-#
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#
-#
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
